9b6c2dc0519b4fafb96fa4c7dbc19bc83b0fb6aa
mirxia <3803518+mirxia@users.noreply.github.com>
2026-07-23 15:40:11 +0800

    Add button to prompt for new grid size

--- script.js
+++ script.js
@@ -20,3 +20,24 @@ function generateCells(width) {
 }
 
 generateCells(16);
+
+const button = document.querySelector("#SetSizeButton");
+button.addEventListener("click", e => {
+    let width = parseInt(prompt("Enter grid with (max 100): ",""));
+    while (width === NaN || width < 1 || width > 100) {
+        switch(true) {
+            case (width === NaN):
+                width = parseInt(prompt("Not an integer, try again: ",""));
+                break
+            case (width < 1):
+                width = parseInt(prompt("Must be positive, try again: ",""));
+                break
+            case (width > 100):
+                width = parseInt(prompt("Max size is 100, try again: ",""));
+                break
+        }
+        width = parseInt(prompt("Invalid value (max 100): ",""));
+    }
+    container.replaceChildren();
+    generateCells(width);
+});
